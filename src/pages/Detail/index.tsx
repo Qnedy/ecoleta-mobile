b2e0d07f9540577b43d834d2f3e54ce3c3d17bb9
qnedy <altmaisefequatro@gmail.com>
2020-06-07 19:55:45 -0300

    home e navigation ok

---
+++ src/pages/Detail/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Image, StyleSheet, Text, ImageBackground } from 'react-native';
+
+const Detail = () => {
+  return (
+   <h1>Detail</h1>
+  )
+};
+
+export default Detail;
---
+++ src/pages/Points/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Image, StyleSheet, Text, ImageBackground } from 'react-native';
+
+const Points = () => {
+  return (
+   <Text>Points</Text>
+  )
+};
+
+export default Points;
